220eeb4454536d49c10fba3f4b93f54d38509662
noony9 <noony9@users.noreply.github.com>
2019-10-12 21:26:28 +0300

    profile page user profile customization form

---
+++ script.js
@@ -0,0 +1,9 @@
+function mainNav() {
+    let x = document.getElementById("topNav");
+        if (x.className === "topnav") {
+        x.className += "responsive";
+    }
+    else {
+        x.className = "topnav";
+    }
+}
